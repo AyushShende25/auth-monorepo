479fac588d0dddb38ad0af9954ec2d84488162ab
AyushShende25 <ayushshende9+git@gmail.com>
2025-05-06 23:02:16 +0530

    feat(web): shadcn setup with tanstack-router

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -5,5 +6,9 @@ export const Route = createFileRoute("/")({
 });
 
 function App() {
-  return <div>Home Page</div>;
+  return (
+    <div className="">
+      <Button>Home</Button>
+    </div>
+  );
 }
--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,11 +1,13 @@
-import { resolve } from "node:path";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+import { resolve } from "node:path";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
